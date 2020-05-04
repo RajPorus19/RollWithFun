e3307aef4e99660919c1852c84bb08d3dbe039a7
RajPorus19 <raj.porus.hiruthayaraj@gmail.com>
2020-05-04 13:02:27 +0200

    added home

---
+++ lib/pages/home.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:roll_with_fun/language.dart';
+
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Home"),
+        centerTitle: true,
+      ),
+    );
+  }
+}
--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:roll_with_fun/language.dart';
+import 'package:roll_with_fun/pages/home.dart';
 
 class Login extends StatelessWidget {
   @override
@@ -15,11 +16,34 @@ class Login extends StatelessWidget {
         Flex(
           direction: Axis.vertical,
           children: <Widget>[
-            Text(DemoLocalizations.of(context).getText('emailHint')),
-            Text(DemoLocalizations.of(context).getText('emailHint2')),
-            Text(DemoLocalizations.of(context).getText('passwordHint')),
-            Text(DemoLocalizations.of(context).getText('passowrdHint2')),
-            Text(DemoLocalizations.of(context).getText('loginText')),
+            SizedBox(height: 100,),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 50,vertical: 10),
+              child: TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: DemoLocalizations.of(context).getText('emailHint'),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 50),
+              child: TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: DemoLocalizations.of(context).getText('passwordHint'),
+                ),
+              ),
+            ),
+            FlatButton(
+              onPressed: () {
+                Navigator.push(context,MaterialPageRoute(builder: (context)=> Home()));
+              }, 
+              child: Text(
+                DemoLocalizations.of(context).getText('loginText'),
+              ),
+            )
           ],
         ),
     );
