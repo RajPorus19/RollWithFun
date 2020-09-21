eb3d9323a60cfb67c2aa7f6b4daeecb888bdf205
RajPorus19 <raj.porus.hiruthayaraj@gmail.com>
2020-09-21 10:37:34 +0200

    Animation in authentification

--- lib/Screens/Authenticate/register.dart
+++ lib/Screens/Authenticate/register.dart
@@ -1,6 +1,8 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:newApp/Models/user.dart';
 import 'package:newApp/Services/auth.dart';
+
 
 class Register extends StatefulWidget {
   // Function toggleView passed through autheticate.dart
@@ -10,7 +12,7 @@ class Register extends StatefulWidget {
   _RegisterState createState() => _RegisterState();
 }
 
-class _RegisterState extends State<Register> {
+class _RegisterState extends State<Register> with TickerProviderStateMixin {
 
   final AuthService _auth = AuthService();
   final _formkey = GlobalKey<FormState>();
@@ -43,6 +45,35 @@ class _RegisterState extends State<Register> {
     return password.length < 6 ? 'Enter a password 6+ chars long' : null;
   }
 
+  // animation
+  Animation<double> animation;
+  AnimationStatus animationStatus;
+  AnimationController animationController;
+  @override
+  void initState(){
+    super.initState();
+    animationController = AnimationController(
+      duration: Duration(milliseconds: 500),
+      vsync: this
+    );
+
+    animation = Tween<double>(begin: 0.0, end: 1.0).animate(animationController)
+    ..addListener(() {
+      setState(() {});
+    })..addStatusListener((status) {
+      setState(() {
+        animationStatus = status;
+      });
+    });
+
+    animationController.forward();
+    
+  }
+
+  startTime() async {
+  var duration = new Duration(milliseconds: 500);
+  return new Timer(duration, widget.toggleView);
+}
 
   @override
   Widget build(BuildContext context) {
@@ -52,81 +83,87 @@ class _RegisterState extends State<Register> {
         elevation: 0.0,
         title: Text('Sign in to Brew Crew'),
         actions: <Widget>[
-          FlatButton.icon(onPressed: widget.toggleView, 
+          FlatButton.icon(onPressed: () async{
+              animationController.reverse();
+              startTime();
+            }, 
           icon: Icon(Icons.arrow_back), 
           label: Text("Sign in"))
         ],
       ),
       body: SingleChildScrollView(
-              child: Container(
+              child: Opacity(
+                opacity: animation.value,
+                child: Container(
           padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
           child: Form(
             key: _formkey,
             child: Column(
-              children: <Widget>[
-                SizedBox(height: 20.0),
-                Text("Register now"),
-                SizedBox(height: 20.0),
+                children: <Widget>[
+                  SizedBox(height: 20.0),
+                  Text("Register now"),
+                  SizedBox(height: 20.0),
+                    TextFormField(
+                  validator: (val) => validateUsername(val),
+                      decoration: InputDecoration(
+                        icon: Icon(Icons.face),
+                        hintText: "Please enter a username",
+                        labelText: "Username",
+                      ),
+                      onChanged: (val) {
+                        setState(() => username = val);
+                      },
+                    ),
+                  SizedBox(height: 20.0),
                   TextFormField(
-                validator: (val) => validateUsername(val),
+                  validator: (val) => validateEmail(val),
                     decoration: InputDecoration(
-                      icon: Icon(Icons.face),
-                      hintText: "Please enter a username",
-                      labelText: "Username",
+                      icon: Icon(Icons.mail),
+                      hintText: "Please enter your email",
+                      labelText: "Email",
                     ),
                     onChanged: (val) {
-                      setState(() => username = val);
+                      setState(() => email = val);
                     },
                   ),
-                SizedBox(height: 20.0),
-                TextFormField(
-                validator: (val) => validateEmail(val),
+                  SizedBox(height: 20.0),
+                  TextFormField(
+                  validator: (val) => validatePassword(val),
                   decoration: InputDecoration(
-                    icon: Icon(Icons.mail),
-                    hintText: "Please enter your email",
-                    labelText: "Email",
+                      icon: Icon(Icons.lock),
+                      hintText: "Please enter a password",
+                      labelText: "Password",
+                    ),
+                    obscureText: true,
+                    onChanged: (val) {
+                      setState(() => password = val);
+                    },
                   ),
-                  onChanged: (val) {
-                    setState(() => email = val);
-                  },
-                ),
-                SizedBox(height: 20.0),
-                TextFormField(
-                validator: (val) => validatePassword(val),
-                decoration: InputDecoration(
-                    icon: Icon(Icons.lock),
-                    hintText: "Please enter a password",
-                    labelText: "Password",
-                  ),
-                  obscureText: true,
-                  onChanged: (val) {
-                    setState(() => password = val);
-                  },
-                ),
-                SizedBox(height: 20.0),
-                RaisedButton(
-                  child: Text(
-                    'Register',
-                  ),
-                  onPressed: () async {
-                    if(_formkey.currentState.validate()){
-                      print(username);
-                      User result = await _auth.registerWithEmailAndPassword(
-                        email, password, username);
-                      if(result == null){
-                        setState(() {
-                          error = "Please provide a valid email.";
-                        });
+                  SizedBox(height: 20.0),
+                  RaisedButton(
+                    child: Text(
+                      'Register',
+                    ),
+                    onPressed: () async {
+                      if(_formkey.currentState.validate()){
+                        print(username);
+                        User result = await _auth.registerWithEmailAndPassword(
+                          email, password, username);
+                        if(result == null){
+                          setState(() {
+                            error = "Please provide a valid email.";
+                          });
+                        }
                       }
-                    }
 
-                  }
-                ),
-                Text(error),
-              ],
+                    }
+                  ),
+                  Text(error),
+                ],
             ),
           ),
         ),
+              ),
       ),
     );
   }
--- lib/Screens/Authenticate/sign_in.dart
+++ lib/Screens/Authenticate/sign_in.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:newApp/Services/auth.dart';
 import 'package:newApp/Shared/loading.dart';
@@ -10,7 +12,7 @@ class SignIn extends StatefulWidget {
   _SignInState createState() => _SignInState();
 }
 
-class _SignInState extends State<SignIn> {
+class _SignInState extends State<SignIn> with TickerProviderStateMixin {
 
   final AuthService _auth = AuthService();
   final _formkey = GlobalKey<FormState>();
@@ -31,6 +33,36 @@ class _SignInState extends State<SignIn> {
     return password.isEmpty ? 'Enter a password' : null;
   }
 
+    // animation
+  Animation<double> animation;
+  AnimationStatus animationStatus;
+  AnimationController animationController;
+  @override
+  void initState(){
+    super.initState();
+    animationController = AnimationController(
+      duration: Duration(milliseconds: 500),
+      vsync: this
+    );
+
+    animation = Tween<double>(begin: 0.0, end: 1.0).animate(animationController)
+    ..addListener(() {
+      setState(() {});
+    })..addStatusListener((status) {
+      setState(() {
+        animationStatus = status;
+      });
+    });
+
+    animationController.forward();
+    
+  }
+
+  startTime() async {
+  var duration = new Duration(milliseconds: 500);
+  return new Timer(duration, widget.toggleView);
+}
+
   @override
   Widget build(BuildContext context) {
     return loading ? Loading() : Scaffold(
@@ -40,70 +72,76 @@ class _SignInState extends State<SignIn> {
         title: Text('Sign in to Brew Crew'),
         //Toggle sign in/register
         actions: <Widget>[
-          FlatButton.icon(onPressed: widget.toggleView, 
+          FlatButton.icon(onPressed: () async{
+              animationController.reverse();
+              startTime();
+            }, 
           icon: Icon(Icons.arrow_forward), 
           label: Text("Register"))
         ],
       ),
       body: SingleChildScrollView(
-              child: Container(
+              child: Opacity(
+                opacity: animation.value,
+                              child: Container(
           padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
           child: Form(
             key: _formkey,
             child: Column(
-              children: <Widget>[
-                SizedBox(height: 20.0),
-                Text("Sign in now"),
-                SizedBox(height: 20.0),
-                TextFormField(
-                  validator: (val) => validateEmail(val),
+                children: <Widget>[
+                  SizedBox(height: 20.0),
+                  Text("Sign in now"),
+                  SizedBox(height: 20.0),
+                  TextFormField(
+                    validator: (val) => validateEmail(val),
+                    decoration: InputDecoration(
+                      icon: Icon(Icons.mail),
+                      hintText: "Please enter your email",
+                      labelText: "Email",
+                    ),
+                    onChanged: (val) {
+                      setState(() => email = val);
+                    },
+                  ),
+                  SizedBox(height: 20.0),
+                  TextFormField(
+                  validator: (val) => validatePassword(password),
                   decoration: InputDecoration(
-                    icon: Icon(Icons.mail),
-                    hintText: "Please enter your email",
-                    labelText: "Email",
-                  ),
-                  onChanged: (val) {
-                    setState(() => email = val);
-                  },
-                ),
-                SizedBox(height: 20.0),
-                TextFormField(
-                validator: (val) => validatePassword(password),
-                decoration: InputDecoration(
-                    icon: Icon(Icons.lock),
-                    hintText: "Please enter a password",
-                    labelText: "Password",
-                  ),
-                  obscureText: true,
-                  onChanged: (val) {
-                    setState(() => password = val);
-                  },),
-                SizedBox(height: 20.0),
-                RaisedButton(
-                  child: Text(
-                    'Sign In',
-                  ),
-                  onPressed: () async {
-                    if(_formkey.currentState.validate()){
-                      setState(() {
-                        loading = true;
-                      });
-                      dynamic result = await _auth.signInWithEmailAndPassword(
-                        email, password);
-                      if(result==null){
+                      icon: Icon(Icons.lock),
+                      hintText: "Please enter a password",
+                      labelText: "Password",
+                    ),
+                    obscureText: true,
+                    onChanged: (val) {
+                      setState(() => password = val);
+                    },),
+                  SizedBox(height: 20.0),
+                  RaisedButton(
+                    child: Text(
+                      'Sign In',
+                    ),
+                    onPressed: () async {
+                      if(_formkey.currentState.validate()){
                         setState(() {
-                          error = "Could not sign in with those credentials";
-                          loading = false;
+                          loading = true;
                         });
+                        dynamic result = await _auth.signInWithEmailAndPassword(
+                          email, password);
+                        if(result==null){
+                          setState(() {
+                            error = "Could not sign in with those credentials";
+                            loading = false;
+                          });
+                        }
                       }
                     }
-                  }
-                ),
-                Text(error),
-              ],
+                  ),
+                  Text(error),
+                ],
             ),
           ),
         ),
+              ),
       ),
     );
   }
